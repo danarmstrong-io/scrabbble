8bb3f2a92e5daa5dc6f3791a03f4e84f606d9f35
Matt Didier <mdidier9@gmail.com>
2014-05-14 17:17:33 -0500

    update for standup

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,5 +1,6 @@
 class Game < ActiveRecord::Base
   has_many :playergames
+  has_many :players, through: :playergames
   has_many :gametiles
   has_many :cells
   belongs_to :winner, class_name: "Player"
--- spec/models/models_spec.rb
+++ spec/models/models_spec.rb
@@ -1,10 +1,21 @@
 require "spec_helper"
 
-describe Player do
+describe "All Models" do
 	let(:p1) { Player.create!(username: "Bob", password: "password", email: "test@test.com") }
+	# let(:g1) {}
 
+	context Player do
 		it "creates and instance of Player" do
 			expect(p1).to be_an_instance_of Player
 		end
+
+		it "should raise an ArgumentError if not passed a username" do
+      expect{ Player.new() }.should be_valid
+    end
+
+		it "has a collection of Game objects" do
+			expect(p1.games.all.to_a).to be_a_kind_of Array
+		end
+	end
 
 end
