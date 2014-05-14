eb7fd8dfa0c178d7e85636f7a1c2eaa93cfe811f
Jason Chodera <jason.chodera@outlook.com>
2014-05-14 15:02:38 -0500

    added Bcrypt auth info to gemfile and player model

--- app/models/player.rb
+++ app/models/player.rb
@@ -2,4 +2,21 @@ class Player < ActiveRecord::Base
   has_many :playergames
   has_many :games, through: :playergames
   has_many :wins, class_name "Game"
+include BCrypt
+
+  def password
+    @password ||= Pawword.new(password_digest)
+  end
+
+  def password=(pass)
+    @entered_password = pass
+    @password = Password.create(pass)
+    self.password_digest = @password
+  end
+
+  def self.authenticate(username, password)
+    user = User.find_by_username(username)
+    return user if user && (user.password == password)
+    nil
+  end
 end
