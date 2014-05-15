aa965239ad570ea09a8a27a45cbe410736428b94
Matt Didier <mdidier9@gmail.com>
2014-05-14 19:38:34 -0500

    updated model specs

--- spec/models/models_spec.rb
+++ spec/models/models_spec.rb
@@ -1,8 +1,8 @@
 require "spec_helper"
 
-describe "All Models" do
+describe "All Models -" do
 	let(:p1) { Player.create!(username: "Bob", password: "password", email: "test@test.com") }
-	# let(:g1) {}
+	let(:g1) { Game.create! }
 
 	context Player do
 		it "creates and instance of Player" do
@@ -13,11 +13,52 @@ describe "All Models" do
       Player.new.should have(1).error_on(:username)
     end
 
-		it "has a collection of Game objects" do
+		it "should have a collection of Game objects" do
 			expect(p1.games.to_a).to be_a_kind_of Array
 		end
 	end
 
-	context Player
+	context Game do
+		it "has a collection of gametiles" do
+			expect(g1.gametiles.to_a).to be_a_kind_of Array
+		end
+
+		it "has a collection of players through player games" do
+			expect(g1.players.to_a).to be_a_kind_of Array
+		end
+
+		it "defaults status to pending after save" do
+			expect(g1.status).to eq "pending"
+		end
+	end
+
+	context Playergame do
+		it "belongs to a Player" do
+			pg1 = p1.playergames.create!
+			expect(pg1.player.username).to eq "Bob"
+		end
+
+		it "belongs to a Game" do
+			pg1 = g1.playergames.create!
+			g1_id = g1.id
+			expect(pg1.game.id).to eq g1_id
+		end
+
+	end
+
+	context Gametile do
+		it "belongs to a game"
+
+		it "belongs to a cell"
+
+		it "belongs to a tile"
+
+		it "belongs to a playergame"
+
+		it "defaults to a tile value of nil" 
+
+		it "requires a cell at creation"
+
+	end
 
 end
