2366a5decc8a008e0120bc89b24dbd41162ae193
Apprentice <apprentice@dbc04.com>
2014-05-15 13:53:04 -0500

    updated jqui functionality)

--- app/assets/javascripts/draggable.js
+++ app/assets/javascripts/draggable.js
@@ -1,46 +1,24 @@
+// Instance variables
 var current_tile;
 var current_cell;
 
 var make_tray_tiles_draggable = function() {
   return $('#tray .tile').draggable({
     snap: '.cell',
-    revert: 'invalid'
-  });
+    revert: 'valid' });
 };
 
 var make_cells_droppable = function() {
   $('.cell').droppable({
     drop: function() {
-      current_tile.css( {left: '0px', top: '0px'} );
-      current_tile.detach().appendTo(this);
-    }
+      if ($(this).children().length == 0) {
+        current_tile.css( {left: '0px', top: '0px'} );
+        current_tile.detach().appendTo(this); } }
   });
 };
 
 var on_tile_grab = function(f) { $('.tile').on( 'mousedown', f ) };
-var define_current_tile = function(e) {
-  // e.preventDefault();
-  current_tile = $(this);
-  console.log(current_tile);
-};
-
-// var on_enter_cell = function(f) { $('.cell').on( 'mouseenter', f ) };
-// var define_current_cell = function(e) {
-//   // e.preventDefault();
-//   current_cell = $(this);
-//   console.log(current_cell);
-// };
-
-// var on_drop_tile = function(e) {
-  // listen for tile drop event
-  // trigger detach/appentTo response
-
-
-// }
-
-
-
-$('#tray .tile').first().detach().appendTo($('#cell_0x0'))
+var define_current_tile = function(e) { current_tile = $(this) };
 
 $(function() {
   make_tray_tiles_draggable();
