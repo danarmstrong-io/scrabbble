cf427ac8a3029845cd147b9e0f6b1d50a4e1ae1f
Ron Gierlach <rongierlach@yahoo.com>
2014-05-14 20:38:20 -0500

    Draggable tiles, still working on snapping and reverting

--- app/assets/javascripts/draggable.js
+++ app/assets/javascripts/draggable.js
@@ -1,3 +1,23 @@
-$(function() {
+var make_tray_tiles_draggable = function() {
+  return $('#tray .tile').draggable({
+    snap: '.cell',
+    revert: 'invalid'
+  });
+};
 
+var make_board_cells_droppable = function() {
+  return $('#board .cell').droppable({
+  });
+};
+
+var make_tray_droppable = function() {
+  return $('#tray').droppable({
+  });
+};
+
+$(function() {
+  alert('page is ready');
+  make_tray_tiles_draggable();
+  make_tray_tiles_draggable();
+  make_board_cells_droppable();
 });
