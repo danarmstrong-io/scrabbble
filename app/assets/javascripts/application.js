4246d220ccab0e7322e84b8d9f014e361d52bbc0
wtfdaboo <wtfdaboo@gmail.com>
2014-05-16 09:38:55 -0500

    Update application.js

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,6 +14,5 @@
 //= require jquery_ujs
 //= require jquery.turbolinks
 //= require turbolinks
-//= require_tree .
-// CODE REVIEW: You probably mean to include this before require_tree
 //= require jquery.ui.all
+//= require_tree .
