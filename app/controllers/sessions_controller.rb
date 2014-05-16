a5a43564c31f69a183605ae7b80e886289289adb
M Didier <mdidier9@gmail.com>
2014-05-16 09:32:35 -0500

    Update sessions_controller.rb

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,9 +3,8 @@ class SessionsController < ApplicationController
   end
 
   def create
-    # CODE REVIEW: you're not using the return value of Player.authenticate
-    if Player.authenticate(params[:username], params[:password])
-      @player = Player.find_by_username(params[:username])
+    @player = Player.authenticate(params[:username], params[:password])
+    if @player
       session[:id] = @player.id
       redirect_to root_path
     else
