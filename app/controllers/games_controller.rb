fe3990e40aac4a607980070e8fde0409b682c50d
Daniel Armstrong <danielarmstrong@Daniels-MacBook-Pro.local>
2014-05-17 15:50:54 -0500

    restful routes

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,13 +1,16 @@
 class GamesController < ApplicationController
   def index
-    @games = Game.where(status: "pending")
+    @games = Game.pending
     @player = Player.find(session[:id]) if session[:id]
   end
 
   def create
-    @game = Game.create(status: "pending")
+    @game = Game.create
+    # CODE REVIEW: this can be done a bit better
+    # http://guides.rubyonrails.org/association_basics.html#has-many-association-reference
     Playergame.create(game: @game, player_id: session[:id], score: 0)
     @tiles = Tile.all
+    # CODE REVIEW: Same as line 11 for the 16-18
     @tiles.each do |tile|
       Gametile.create(tile: tile, game: @game)
     end
@@ -15,7 +18,7 @@ class GamesController < ApplicationController
   end
 
   def join
-    @game = Game.find(params[:game_id])
+    @game = Game.find(params[:id])
     Playergame.create(game: @game, player_id: session[:id], score: 0)
     @game.status = "active"
     @game.save
